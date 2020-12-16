8b1cbc87e561953ce6f261410c9dc3b0d2b1bd85
Jhon Calderón <jhoncv@digitalware.com.co>
2020-12-15 20:09:02 -0500

    Unit of Work test

---
+++ src/AspNetClient/Controllers/ValuesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Services;
+
+namespace AspNetClient.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IInvoiceService _invoiceService;
+
+        public ValuesController(IInvoiceService invoiceService)
+        {
+            _invoiceService = invoiceService;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return Ok(
+                _invoiceService.GetAll()
+            );
+        }
+    }
+}
---
+++ src/Common/Parameters.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Common
+{
+    public class Parameters
+    {
+        public const string ConnectionString = "Server=DESKTOP-GI4SU4B\\SQLEXPRESS;Initial Catalog=KodotiSells;Integrated Security=true;";
+        public const decimal IvaRate = 0.18m;
+    }
+}
